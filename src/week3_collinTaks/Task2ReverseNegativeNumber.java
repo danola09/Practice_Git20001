de5ebfece4c1236ccf211bfa4f395dfadec59edf
VMWarePratice <dano_la@yahoo.com>
2023-01-06 21:30:11 -0500

    week3 Collin's Task #2

---
+++ src/week3_collinTaks/Task2ReverseNegativeNumber.java
@@ -0,0 +1,18 @@
+package week3_collinTaks;
+
+public class Task2ReverseNegativeNumber {
+    public static void main(String[] args) {
+
+
+
+        int a = -5;
+        System.out.println(-(a));
+
+
+    }
+    public static int reverseNegative(int a){
+        int b = (-(a));
+        return b;
+    }
+
+}
